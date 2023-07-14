64c5d3e92453c2ccf268463c13ecf7e3e594e6b1
Keerthana607 <87594972+Keerthana607@users.noreply.github.com>
2023-07-14 10:55:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,21 +15,14 @@ app.use(
   })
 )
 app.use('/public', express.static('public'));
-const mySecret = process.env['mongoUrl']
- //Database 
-const intialDbConnection = async () => {
-  try {
-    await mongoose.connect(mySecret, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    console.log("db connected")
-    
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGOCONNECTION, { useNewUrlParser: true });
+
+
+app.listen(process.env.PORT, () => {
+    console.log("App listening on port " + process.env.PORT);
+})
   app.use(bodyParser.urlencoded({ extended: false })) 
   app.use(bodyParser.json()) 
 var Users = require('./routes/Users')
@@ -38,7 +31,4 @@ var Tickets = require('./routes/Tickets')
 app.use('/users', Users)
 app.use('/tickets', Tickets)
 
-var server=app.listen(port, function() {
-  var host=server.address().address;
-  console.log('Server is running on port: ' + host,port)
-})
+
